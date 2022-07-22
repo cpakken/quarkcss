cb734dfe983e8e81d1001df656368d0f05a593e3
Chris Pakken <cpakken@gmail.com>
2022-07-21 23:45:18 -0700

    react tests

--- packages/react/src/styled.test.tsx
+++ packages/react/src/styled.test.tsx
@@ -1,7 +1,36 @@
+// @vitest-environment happy-dom
+
+import React from 'react'
 import { styled } from '.'
+import { render } from '@testing-library/react'
 
 describe('styled', () => {
+  const Container = styled('div', {
+    base: 'baseClass',
+    variants: {
+      color: { red: 'red', blue: 'blue' },
+      size: { small: 'small', large: 'large' },
+    },
+    defaultVariants: {
+      color: 'blue',
+    },
+  })
+
   test('basic', () => {
-    const Container = styled('div', {})
+    const { container } = render(<Container />)
+    const { className } = container.firstElementChild!
+    expect(className).toEqual('baseClass blue')
+  })
+
+  test('props', () => {
+    const { container } = render(<Container color="red" size="large" />)
+    const { className } = container.firstElementChild!
+    expect(className).toEqual('baseClass red large')
+  })
+
+  test('custom className', () => {
+    const { container } = render(<Container color="red" size="large" className="custom" />)
+    const { className } = container.firstElementChild!
+    expect(className).toEqual('custom baseClass red large')
   })
 })
