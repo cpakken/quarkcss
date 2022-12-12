780ab90f2572fb86771cee11e774215790ceb1b4
Chris Pakken <cpakken@gmail.com>
2022-12-12 12:11:15 -0800

    compoundVariants and null variant key for inverse

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,42 +1,79 @@
-export type QuarkVariants = { [key: string]: string }
+//null variant is applied when no variant value is passed
+export type QuarkVariants = { [key: string]: string | string[] } & { null?: string | string[] }
+// export type QuarkVariants = { [key: string]: string }
 
 export type QuarkVariantsMap = { [variantKey: string]: QuarkVariants }
 
-type TrueStringToBoolean<StringUnion> = StringUnion extends 'true'
-  ? Exclude<StringUnion, 'true'> | boolean
+type TrueStringToBoolean<StringUnion> = StringUnion extends 'true' | 'null'
+  ? Exclude<StringUnion, 'true' | 'null'> | boolean | null | undefined
   : StringUnion
 
 export type PropsOfVariantsMap<VariantsMap extends QuarkVariantsMap> = {
-  [Key in keyof VariantsMap]?: TrueStringToBoolean<keyof VariantsMap[Key]>
+  [Key in keyof VariantsMap]?: TrueStringToBoolean<keyof VariantsMap[Key] & string>
 }
 
 export type DefaultVariants<VariantsMap extends QuarkVariantsMap> = {
-  [key in keyof VariantsMap]?: keyof VariantsMap[key] & string
+  [key in keyof VariantsMap]?: TrueStringToBoolean<keyof VariantsMap[key] & string>
 }
+
+export type CompoundVariant<VariantsMap extends QuarkVariantsMap> = {
+  [key in keyof VariantsMap]?: TrueStringToBoolean<keyof VariantsMap[key] & string>
+} & { value: string | string[] }
 
 export type QuarkConfig<VariantsMap extends QuarkVariantsMap = {}> = {
   base?: string | string[]
   variants?: VariantsMap
+  compoundVariants?: CompoundVariant<VariantsMap>[]
   defaultVariants?: DefaultVariants<VariantsMap>
 }
 
-export type QuarkCss<VariantsMap extends QuarkVariantsMap> = (variantValues?: PropsOfVariantsMap<VariantsMap>) => string
+export type QuarkCss<VariantsMap extends QuarkVariantsMap> = (
+  variantValues?: PropsOfVariantsMap<VariantsMap>
+) => string
 
-export function css<VariantsMap extends QuarkVariantsMap>(config: QuarkConfig<VariantsMap>): QuarkCss<VariantsMap> {
-  const { base, variants, defaultVariants } = config
+export function css<VariantsMap extends QuarkVariantsMap>(
+  config: QuarkConfig<VariantsMap>
+): QuarkCss<VariantsMap> {
+  const { base, variants, defaultVariants, compoundVariants = [] } = config
   const baseClass = Array.isArray(base) ? base.join(' ') : base
   const variantsEntries = Object.entries(variants || {})
 
   return (props: PropsOfVariantsMap<VariantsMap> = {}) => {
     const classNames: string[] = baseClass ? [baseClass] : []
 
+    //Process Variants
     for (const [key, map] of variantsEntries) {
-      const variantProp = props[key]
-      const className = variantProp ? map[variantProp.toString()] : defaultVariants?.[key]
+      const className = map[keyToString(props[key])] ?? defaultVariants?.[key]
 
-      if (className) classNames.push(className)
+      if (className) classNames.push(...arrayify(className))
+    }
+
+    //Process Compound Variants
+    for (const variant of compoundVariants) {
+      let match = true
+
+      for (const key in variant) {
+        const propKey = keyToString(key)
+
+        if (propKey === 'value') continue
+        if (props[propKey] !== variant[propKey]) {
+          match = false
+          break
+        }
+      }
+
+      if (match) {
+        classNames.push(...arrayify(variant.value))
+      }
     }
 
     return classNames.join(' ')
   }
+}
+
+const arrayify = <T>(value: T | T[]): T[] => (Array.isArray(value) ? value : [value])
+
+const keyToString = (key: string | boolean | null | undefined): string => {
+  //If falsey, return 'null' as the prop key
+  return !key ? 'null' : key.toString()
 }
--- packages/core/src/tests/css.test.ts
+++ packages/core/src/tests/css.test.ts
@@ -34,4 +34,50 @@ describe('core', () => {
     expect(className({ color: 'red' })).toEqual('baseClass red')
     expect(className({ size: 'large' })).toEqual('baseClass blue large')
   })
+
+  test('true boolean', () => {
+    const className = css({
+      base: 'baseClass',
+      variants: {
+        isDragging: { true: 'dragging' },
+        hover: { null: 'notHover' },
+      },
+    })
+
+    expect(className({ isDragging: true })).toEqual('baseClass dragging notHover')
+    expect(className({ isDragging: true, hover: false })).toEqual('baseClass dragging notHover')
+    expect(className({ isDragging: true, hover: true })).toEqual('baseClass dragging')
+  })
+
+  test('compoundVariants', () => {
+    const className = css({
+      base: 'baseClass',
+      variants: {
+        color: { red: 'red', blue: 'blue' },
+        size: { small: 'small', large: 'large' },
+        isDragging: { true: 'dragging' },
+      },
+      compoundVariants: [
+        {
+          color: 'red',
+          size: 'small',
+          value: 'redSmall',
+        },
+        {
+          color: 'blue',
+          size: 'large',
+          value: 'blueLarge',
+        },
+        {
+          color: 'blue',
+          isDragging: true,
+          value: 'blueDragging',
+        },
+      ],
+    })
+
+    expect(className({ color: 'red', size: 'small' })).toEqual('baseClass red small redSmall')
+    expect(className({ color: 'blue', size: 'large' })).toEqual('baseClass blue large blueLarge')
+    expect(className({ color: 'blue', isDragging: true })).toEqual('baseClass blue dragging blueDragging')
+  })
 })
